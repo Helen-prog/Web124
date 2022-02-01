09d0dc8faba3b63a8843f006cc7989185dfc56f3
Helen <elena.kozovyakina@gmail.com>
2022-02-01 16:29:41 +0300

    project complete

---
+++ src/components/MovieList.js
@@ -0,0 +1,18 @@
+import Movie from "./Movie";
+import "./MovieList.css";
+
+function MovieList(props) {
+    const { movies = [] } = props;
+
+    return (
+        <div className="movies">
+            {
+                movies.length ? movies.map(movie => {
+                    return <Movie key={movie.imdbID} {...movie} />
+                }) : <h2>Nothing found</h2>
+            }
+        </div>
+    )
+}
+
+export default MovieList;
